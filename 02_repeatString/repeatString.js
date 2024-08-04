168c616a905e5cdf5b8c34ea9c39121b1a5ad0f8
Jia Jun <jackiewu099@gmail.com>
2024-08-04 00:49:44 -0400

    Solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    else if (string == "") {
+        return "";
+    }
 
+    ans = "";
+    for (let i = 0; i < num; i++) {
+        ans = ans.concat(string);
+    }
+    return ans;
 };
 
 // Do not edit below this line
