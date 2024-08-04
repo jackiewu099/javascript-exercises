0e940e8c76b162d67899640dacad1b9ee383ff67
Jia Jun <jackiewu099@gmail.com>
2024-08-04 01:10:08 -0400

    Solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let array = [];
+    for (let i = 0 ; i < word.length; i++) {
+        array.push(word[i]);
+    }
 
+    let l = 0;
+    let r = array.length-1;
+    
+    while (l < r) {
+        [array[l], array[r]] = [array[r], array[l]];
+        l++;
+        r--;
+    }
+    word = array.join('');
+    return word;
 };
 
 // Do not edit below this line
