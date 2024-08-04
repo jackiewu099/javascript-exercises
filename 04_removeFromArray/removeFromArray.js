a4fdb00bb119e68cef8a5034add7fd40dd1bfa1e
Jia Jun <jackiewu099@gmail.com>
2024-08-04 01:52:32 -0400

    Solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+
+    const set1 = new Set();
+    for (let i = 1; i < arguments.length; i++) {
+        set1.add(arguments[i]);
+    }
+
+    for (let j = array.length; j >= 0; j--) {
+        if (set1.has(array[j])) {
+            array.splice(j, 1);
+        }
+    }
+    return array;
+
 };
 
 // Do not edit below this line
