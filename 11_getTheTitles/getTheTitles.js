c7a031d840185997377104c5af8d555b8fe5e073
Jia Jun <jackiewu099@gmail.com>
2024-08-07 00:49:49 -0400

    Solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+function getTitle(book) {
+    return book.title;
+}
 
+const getTheTitles = function(books) {
+
+    const titles = books.map(getTitle);
+    return titles;
 };
 
 // Do not edit below this line
