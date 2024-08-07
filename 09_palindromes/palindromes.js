81fcb7a0382017e7d4ca3fe5fee7ed6429159e52
Jia Jun <jackiewu099@gmail.com>
2024-08-07 00:35:57 -0400

    Solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+function isAlphanumeric(char) {
+    let code = char.charCodeAt(0);
+    return (code >= 48 && code <= 57) || // 0-9
+           (code >= 65 && code <= 90) || // A-Z
+           (code >= 97 && code <= 122);  // a-z
+}
 
+
+const palindromes = function (string) {
+    let l = 0;
+    let r = string.length-1;
+
+    while (l < r) {
+        while (l < r && !isAlphanumeric(string[l])) {
+            l++;
+        }
+        while (l < r && !isAlphanumeric(string[r])) {
+            r--;
+        }
+        if (string[l].toLowerCase() !== string[r].toLowerCase()) {
+            return false;
+        }
+        l++;
+        r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
